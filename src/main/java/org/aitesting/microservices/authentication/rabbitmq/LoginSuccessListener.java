62d11251f62fb9def8a824ec25753400d4fbcc6e
Isabella <irplonk@gatech.edu>
2018-07-20 16:09:18 -0400

    Remove unused import

--- src/main/java/org/aitesting/microservices/authentication/rabbitmq/LoginSuccessListener.java
+++ src/main/java/org/aitesting/microservices/authentication/rabbitmq/LoginSuccessListener.java
@@ -3,7 +3,6 @@ package org.aitesting.microservices.authentication.rabbitmq;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEvent;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.core.userdetails.User;
