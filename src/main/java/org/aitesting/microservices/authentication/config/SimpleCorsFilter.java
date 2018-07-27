9332dc738bd16f3efe84e52d8e77471362206b3d
jusoto <jusoto@gmail.com>
2018-07-27 01:47:38 -0400

    Removed headers for CORS filter due to duplicated response headers on front-end due to edge-service is also adding these headers. Added a try catch for exceptions when sending the message to notification service

--- src/main/java/org/aitesting/microservices/authentication/config/SimpleCorsFilter.java
+++ src/main/java/org/aitesting/microservices/authentication/config/SimpleCorsFilter.java
@@ -26,11 +26,6 @@ public class SimpleCorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization");
 
         HttpServletRequest request = (HttpServletRequest) req;
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
