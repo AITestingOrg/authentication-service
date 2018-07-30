dfbd848f019c757e858e93c2c13c1e10bf4a6d35
Isabella <irplonk@gatech.edu>
2018-07-30 16:10:32 -0400

    Add uuid to access token

--- src/main/java/org/aitesting/microservices/authentication/security/JWTOAuth2Config.java
+++ src/main/java/org/aitesting/microservices/authentication/security/JWTOAuth2Config.java
@@ -3,14 +3,13 @@ package org.aitesting.microservices.authentication.security;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
-import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.*;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
@@ -28,15 +27,17 @@ public class JWTOAuth2Config extends AuthorizationServerConfigurerAdapter {
     @Autowired
     private JwtAccessTokenConverter jwtAccessTokenConverter;
 
-    @Autowired
-    private TokenEnhancer jwtTokenEnhancer;
-
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
-        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(jwtTokenEnhancer, jwtAccessTokenConverter));
-        endpoints.tokenStore(tokenStore).accessTokenConverter(jwtAccessTokenConverter)
+        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(jwtTokenEnhancer(), jwtAccessTokenConverter));
+        endpoints.tokenStore(tokenStore).tokenEnhancer(tokenEnhancerChain)
                 .authenticationManager(authenticationManager).userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public TokenEnhancer jwtTokenEnhancer() {
+        return new JWTTokenEnhancer();
     }
 
 }
--- src/main/java/org/aitesting/microservices/authentication/security/JWTTokenEnhancer.java
+++ src/main/java/org/aitesting/microservices/authentication/security/JWTTokenEnhancer.java
@@ -4,9 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import org.aitesting.microservices.authentication.model.User;
 import org.aitesting.microservices.authentication.model.UserOrganization;
 import org.aitesting.microservices.authentication.repository.OrgUserRepository;
+import org.aitesting.microservices.authentication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
@@ -17,15 +20,25 @@ public class JWTTokenEnhancer implements TokenEnhancer {
     @Autowired
     private OrgUserRepository orgUserRepo;
 
+    @Autowired
+    private UserRepository userRepo;
+
     private UUID getOrgId(String username) {
         UserOrganization orgUser = orgUserRepo.findByUsername(username);
         return orgUser.getOrganizationId();
+    }
+
+    private UUID getUserId(String username) {
+        User user = userRepo.findByUsername(username);
+        return user.getUserId();
     }
 
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
         Map<String, Object> additionalInfo = new HashMap<>();
         UUID orgId = getOrgId(authentication.getName());
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        additionalInfo.put("uuid", getUserId(userDetails.getUsername()));
         additionalInfo.put("organizationId", orgId);
         ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
         return accessToken;
